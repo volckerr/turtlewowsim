236ac66956eae685e48343f666441e34a2a3e395
Pepopo <pepopo978@gmail.com>
2024-03-05 14:43:46 -0800

    add test to easily check different num mages

---
+++ examples/num_mage_test.py
@@ -0,0 +1,13 @@
+from classicmagedps import FireMage, FireEnvironment, Simulation
+
+mages = []
+num_mages = 5
+
+for i in range(num_mages):
+    fm = FireMage(name=f'mage{i}', sp=1004, crit=32, hit=16, fullt2=False)
+    fm.smart_scorch()
+    mages.append(fm)
+
+
+sim = Simulation(env=FireEnvironment, mages=mages)
+sim.run(iterations=1000, duration=300)
